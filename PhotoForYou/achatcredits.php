4fddd1a9677562ee68c5b6ad58da49681742546c
Dimitry.S <skodim@orange.fr>
2019-01-07 04:58:02 +0100

    Update achatcredits.php

--- PhotoForYou/achatcredits.php
+++ PhotoForYou/achatcredits.php
@@ -10,7 +10,7 @@
     <div id="container">
     
         <?php 
-        include_once 'include/entete.inc.php';
+        include_once 'include/entete.php';
         ?>
         
         <div id="body">
@@ -33,7 +33,7 @@
         </div>
 
 
-        <?php include_once 'include/pieddepage.inc.php'; ?>
+        <?php include_once 'include/pieddepage.php'; ?>
         
     </div>
 </body>
