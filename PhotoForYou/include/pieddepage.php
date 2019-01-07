36987fb9c433f17b3a2b804a1213244dbfa25159
Dimitry.S <skodim@orange.fr>
2019-01-07 04:58:47 +0100

    Create pieddepage.php

---
+++ PhotoForYou/include/pieddepage.php
@@ -0,0 +1,19 @@
+<br/>
+<div id="footer">
+    <div class="footer-content">
+       
+        <div class="clear"></div>
+    </div>
+   
+         
+    </div>	
+	
+         <img src="images/footer.jpg" alt="photoPaysage">
+
+		 
+    <div class="footer-bottom">
+        <p>&copy; PhotoForYou 2018. by SKOLIMOWSKI DIMITRY</p>
+     </div>
+</div>
+
+<?php $Connexion=NULL ?><!--Ferme la connection avec la base de donnée-->
